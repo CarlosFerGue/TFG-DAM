4759b1d125c82bad50c82e9f8d89a9c54168271d
Gigantosaurio <jalquezar04@gmail.com>
2024-05-06 19:00:01 +0200

    stash

--- FrontEnd/src/components/Main.jsx
+++ FrontEnd/src/components/Main.jsx
@@ -3,6 +3,7 @@ import Constants from "expo-constants";
 import Background from "./Background"; // Asegúrate de que la ruta de importación es correcta
 import { View, Text, StyleSheet, Image } from "react-native";
 
+
 const Main = () => {
   return (
     <Background>
