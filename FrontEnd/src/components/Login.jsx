cae5e8643dccb6ee403185a6d4b9970c9b5d1276
Carlos Fernández Guevara <131338727+CarlosFerGue@users.noreply.github.com>
2024-05-06 21:10:58 +0200

    Avances en el Login
    Avances socio, no ralles

--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, TextInput, Button } from "react-native";
 import Constants from "expo-constants"; // Asegúrate de importar Constants si lo estás utilizando
 
 const Login = () => {
@@ -14,7 +14,20 @@ const Login = () => {
       <Text style={styles.subtitulo}>
         Tu portal de actividades y experiencias en Zaragoza.
       </Text>
-      <Text style={{ color: "#fff", fontSize: 24 }}>Main</Text>
+      <Text style={styles.inputText}>Nombre de usuario</Text>
+      <TextInput
+        style={styles.inputs}
+        placeholder="Ingresa tu nombre de usuario"
+      />
+      <Text style={styles.inputText}>Contraseña</Text>
+
+      <TextInput style={styles.inputs} placeholder="Ingresa tu contraseña" />
+
+      <Text style={styles.olvidona}>¿Has olvidado tu contraseña?</Text>
+
+      <Button title="Iniciar Sesión" />
+
+      <Text style={styles.crearcuenta}>Crear Una Nueva Cuenta</Text>
     </View>
   );
 };
@@ -23,27 +36,57 @@ const styles = StyleSheet.create({
   container: {
     marginTop: Constants.statusBarHeight,
     flexGrow: 1,
-    justifyContent: "center",
-    alignItems: "center",
     padding: "5%",
+    // borderColor: "#fff",
+    // borderWidth: 1,
+    backgroundColor: "dodgerblue",
+    display: "flex",
   },
   logo: {
     width: "100%",
     maxHeight: "100%",
-    alignItems: "center",
     justifyContent: "center",
+    alignSelf: "flex-start",
+    bottom: "5%",
   },
   bienvenido: {
-    fontSize: 24,
+    fontSize: 35,
     color: "#fff",
+    textAlign: "center",
+    bottom: "14%",
   },
   subtitulo: {
-    fontSize: 24,
+    fontSize: 20,
     color: "#fff",
+    textAlign: "center",
+    bottom: "12%",
   },
-  titulo3: {
-    backgroundColor: "tomato",
-    flex: 1,
+  inputText: {
+    fontSize: 20,
+    color: "#fff",
+    bottom: "3%",
+    marginBottom: "5%",
+  },
+  inputs: {
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 8,
+    width: "100%",
+    bottom: "5%",
+    marginBottom: "1%",
+  },
+  olvidona: {
+    fontSize: 13,
+    color: "#fff",
+    bottom: "3%",
+    textAlign: "center",
+    bottom: "4%",
+  },
+  crearcuenta: {
+    fontSize: 20,
+    color: "#fff",
+    textAlign: "center",
+    top: "13%",
   },
 });
 
