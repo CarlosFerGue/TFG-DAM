0a5e32dac39a44db111cabb0d751aefcd7154280
Gigantosaurio <jalquezar04@gmail.com>
2024-05-06 18:58:30 +0200

    Update App.js

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,6 +1,12 @@
-import React from 'react';
 import Main from './src/components/Main.jsx';
+import React, { useEffect } from 'react';
+import { SplashScreen } from 'expo-splash-screen';
 
 export default function App() {
-  return <Main/>;
+
+  useEffect(() => {
+    setTimeout(async () => {
+      await SplashScreen.hideAsync(); // Oculta el splash screen después de 2 segundos
+    }, 60000); 
+  }, []);
 }
